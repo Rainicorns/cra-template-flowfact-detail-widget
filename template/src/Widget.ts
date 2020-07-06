046acf2401268a7c536d4b85dba8c24431658bee
Daniel Kim <danielkim@Daniels-MacBook-Pro.local>
2020-07-06 12:15:47 +0200

    template

--- template/src/Widget.ts
+++ template/src/Widget.ts
@@ -4,7 +4,7 @@ import './Widget.scss'
 export default function Widget({ entity }) {
     return (
         <div className="MyWidget">
-            <h1>Hello,</h1>
+            <h1>Hello2,</h1>
             <h2>{entity.emails.values[0]}</h2>
         </div>
     );
