77447fd0b2f9e435515254ca98cf6401fa1a4ac7
Daniel Kim <danielkim@Daniels-MacBook-Pro.local>
2020-07-06 15:48:25 +0200

    displayed meaningful error message outside of postrobot

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import postRobot from 'post-robot';
 import { Authentication, EnvironmentManagement, EntityService as Service } from '@flowfact/api-services'
 import { reactLocalStorage } from 'reactjs-localstorage';
+import JSONPretty from 'react-json-pretty';
 import Widget from './Widget';
 import settings from './config/init.json'
 
@@ -10,6 +11,7 @@ function App() {
   const [schemaID, setSchemaID] = useState(0);
   const [isAuth, setIsAuth] = useState(false);
   const [tokens, setTokens] = useState(false);
+  const [outside, setOutside] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -24,17 +26,27 @@ function App() {
     if (reactLocalStorage.getObject('fftokens')) {
       setTokens(reactLocalStorage.getObject('fftokens'));
     }
+
     postRobot.on(`initial_${settings.widget.configuration.moduleKey}`, async ({ data }) => {
       EnvironmentManagement.stage = data.environment.stage
       EnvironmentManagement.version = data.environment.versionTag
       reactLocalStorage.setObject('fftokens', data.tokens);
       setTokens(data.tokens);
     });
-    postRobot.send(window.parent, `ready_${settings.widget.configuration.moduleKey}`, true);
+    console.log("postRobot : ", postRobot);
+    const res = postRobot.send(window.parent, `ready_${settings.widget.configuration.moduleKey}`, true)
+      .catch(er => {
+        console.error("Post Robot : ", er)
+        setOutside(true)
+      })
+
+    console.log("Send : ", res)
+
     postRobot.on(`data_${settings.widget.configuration.moduleKey}`, async (event) => {
       setEntityID(event.data.entityId);
       setSchemaID(event.data.schemaId);
     });
+
   }, [])
 
   function EntityGetter() {
@@ -50,7 +62,14 @@ function App() {
 
   return (
     <>
-      {isAuth && entityID && schemaID && <EntityGetter entityID={entityID} schemaID={schemaID} />}
+      {outside ? (
+        <div>
+          <h1>Seems like you're trying to view the widget outside of FLOWFACT.</h1>
+          <h2>That really isn't going to work. Trying viewing it INSIDE.</h2>
+          <JSONPretty id="json-pretty" data={settings}></JSONPretty>
+        </div>
+      ) :
+        (isAuth && entityID && schemaID) && <EntityGetter entityID={entityID} schemaID={schemaID} />}
     </>
   )
 }
